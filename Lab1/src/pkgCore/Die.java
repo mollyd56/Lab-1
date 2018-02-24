05dcb95010dc1530ea6c058aeebd412820f092b1
Molly Doyle /UD <36247600+mollyd56@users.noreply.github.com>
2018-02-24 14:09:58 -0500

    Update Die.java

--- Lab1/src/pkgCore/Die.java
+++ Lab1/src/pkgCore/Die.java
@@ -6,6 +6,8 @@ public class Die {
 
 	public Die() {
 		// TODO: Determine DieVaue.. a random number between 1 and 6
+		int random = (int)(Math.random()*6 + 1);
+		this.DieValue = random;
 	}
 
 	public int getDieValue() {
