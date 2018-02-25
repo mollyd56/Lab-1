5cfea7dcc5f3314c34626df6729b72efd8ba6874
Molly Doyle /UD <36247600+mollyd56@users.noreply.github.com>
2018-02-25 18:28:23 -0500

    Update Round.java

--- Lab1/src/pkgCore/Round.java
+++ Lab1/src/pkgCore/Round.java
@@ -9,12 +9,25 @@ public class Round {
 	private LinkedList<Roll> rolls = new LinkedList<Roll>();
 
 	public Round() {
+		Roll r = new Roll();
 		// TODO: Execute Come Out roll, value ComeOutScore
-
+		this.ComeOutScore = r.getScore();
+		if(ComeOutScore == 2 ||ComeOutScore == 3 ||ComeOutScore == 12)
+			this.eGameResult = CRAPS;
+		if(ComeOutScore == 7 ||ComeOutScore == 11)
+			this.eGameResult = NATURAL;
 		// TODO: Create a loop that will execute a roll until point is made, or
 		// seven-out
+		while (r.getScore() != ComeOutScore && r.getScore != 7 )
+		{
+			r.Roll();
+		}
 
 		// TODO: value the eGameResult after the round is complete
+		if(r.getScore == ComeOutValue)
+			this.eGameResult = POINT;
+		if(r.getScore == 7)
+			this.eGameResult = SEVEN_OUT;
 	}
 
 	public int RollCount() {
